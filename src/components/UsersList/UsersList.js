511c5de4922c5841bd96301fe979bc27b25b3cc2
MacLeodOne <miron7070@gmail.com>
2021-11-19 11:51:42 +0100

    stylowanie isSecondary

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -2,15 +2,25 @@
 import React from 'react';
 import {users} from 'data/users';
 import UsersLiestItem from 'components/UsersListItem/UsersListItem';
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: white;
+  width: 100%;
+  max-width: 500px;
+  padding: 40px 30px;
+  border-radius: 25px;
+  box-shadow: 0 5px 15px -10px rgba(0, 0, 0, 0.3);
+`;
 
 const UsersList = () => (
-<div>
+<Wrapper>
         <ul>
             {users.map((userData) => (
             <UsersLiestItem userData={userData} />
             ))}
         </ul>
-    </div>
+    </Wrapper>
 );
 
 UsersList.propTyps = {};
--- src/components/UsersListItem/UsersListItem.js
+++ src/components/UsersListItem/UsersListItem.js
@@ -1,34 +1,45 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-/**
- * const UsersLiestItem = (props) =>(
-<li>
-    {console.log(props)}
-                <div>{props.userData.average}</div>
-                <div>
-                    <p> {props.userData.name}</p>
-                    <p>{props.userData.attendance}</p>
-                </div>
-                <button>
-                    X
-                </button>
-            </li>
-);
- */
+
+export const Wrapper = styled.li`
+  display: flex;
+  align-items: center;
+  position: relative;
+  &:not(:last-child)::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 1px;
+    background-color: lightgrey;
+  }
+`;
+
+const StyleButton = styled.button`
+width: 25px;
+height: 25px;
+background-color: ${(props) => (props.isSecondary ? '#e7e044':'#c0c7d6')};
+border-radius:50px;
+border:none;
+color:white;
+
+`;
+
+
 const UsersLiestItem = ({userData: {average,name,attendance = '0%' }}) =>(
-<li>
+<Wrapper>
 
                 <div>{average}</div>
                 <div>
                     <p> {name}</p>
                     <p>{attendance}</p>
                 </div>
-                <button>
-                    X
-                </button>
-            </li>
+                <StyleButton>X</StyleButton>
+                <StyleButton isSecondary>X</StyleButton>
+            </Wrapper>
 );
 
 UsersLiestItem.propTypes = {
